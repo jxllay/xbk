e110652d67838c1996462c1b7be318c7140d59d1
lay <1217173875@qq.com>
2018-08-06 09:53:54 +0800

    one

---
+++ js/tmf.js
@@ -0,0 +1,19 @@
+	$(function(){
+		$(".tu4").mouseover(function(){
+			$(this).addClass("xs");
+		}).mouseleave(function(){
+			$(this).removeClass("xs");
+		})
+		$(".xh").mouseover(function(){
+			$(this).addClass("xs");
+		}).mouseleave(function(){
+			$(this).removeClass("xs");
+		})
+	})
+//window.onresize=r;
+//function r(){
+//	var winW=window.innerWidth;
+//	document.getElementsByTagName("gb")[0].style.fontSize=winW*0.00925+"px";
+//}
+//r();	
+
